66ff35f03cc9e18cd911686ba6364304e55b100b
swchaw <swati.chawla@atmecs.com>
2021-11-08 13:00:52 +0530

    updated add.java

--- src/main/java/Add.java
+++ src/main/java/Add.java
@@ -1,5 +1,10 @@
 public class Add {
   public void addNumbers(int num1, int num2) {
     // testing
+    int num3 = num2-num1;
+    // testing
+  }
+  public void addNumber(int num1, int num2) {
+    // testing
   }
 }
