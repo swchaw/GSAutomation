5cd99d53d0c6be1f6b7e681a89d2459bf206e805
arunatm2017 <48243273+arunatm2017@users.noreply.github.com>
2021-11-08 12:57:03 +0530

    Added sum code

--- src/main/java/Add.java
+++ src/main/java/Add.java
@@ -1,4 +1,9 @@
 public class Add {
   public void addNumbers(int num1, int num2) {
+    int sum = num1 + num2;
+    System.out.println("Sum is: "+sum)
+  }
+  
+  public void multiplyNumbers(int num1, int num2) {
   }
 }
