1a6e7b8f62803b27f0b1081253b3e768f7e9e6ba
swchaw <swati.chawla@atmecs.com>
2021-11-09 14:22:41 +0530

    local updated

--- src/main/java/com/gs/automation/testscripts/GS.java
+++ src/main/java/com/gs/automation/testscripts/GS.java
@@ -21,9 +21,9 @@ public class GS {
         //Setting webdriver.gecko.driver property
         System.setProperty("webdriver.chrome.driver","C:\\Users\\swati.chawla\\DriverCrom\\chromedriver.exe");
         //Instantiating driver object and launching browser
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver1 = new ChromeDriver();
         String url = " https://www.google.com";
-        driver.get(url);
+        driver1.get(url);
         JSONParser parser = new JSONParser();
 
         Object obj = parser.parse(new FileReader("C:\\Users\\swati.chawla\\sample\\src\\main\\java\\com\\gs\\automation\\temp.json"));
@@ -32,10 +32,10 @@ public class GS {
         jsonObject.get("search-text");
         System.out.println(jsonObject.get("search-text"));
 
-        WebElement searchbox= driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+        WebElement searchbox= driver1.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
         searchbox.sendKeys("selenium java search ");
         //Closing the browser
-        WebElement googleSearchbutton= driver.findElement(By.xpath("(//*[@class='gNO89b'])[2]"));
+        WebElement googleSearchbutton= driver1.findElement(By.xpath("(//*[@class='gNO89b'])[2]"));
         googleSearchbutton.click();
         //driver.quit();
     }
